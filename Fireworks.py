bec021e835378cc5f74b53fc1e23a9a86b5362bb
rayestGeeta <rayestgeeta2018@gmail.com>
2020-05-25 08:20:16 +0800

    Class+Driver

--- Fireworks.py
+++ Fireworks.py
@@ -270,19 +270,3 @@ def main(number , cs):
         clock.tick(150)
 
     pygame.quit()
-
-
-if __name__ == "__main__":
-    print('Would you like to customize it？ (yes or no)')
-    f = input()
-    if f == 'yes':
-        number = input('Number of particles in a firework:(Default 100)\n')
-        cs = input('Colors:(Default 2 of them are with single colors (red, green) '
-                   'and the other 3 with randomly generated colors) (style:(1, 0, 0, 1)\n')
-    else:
-        number = 10
-        cs = [(random.random(), random.random(), random.random(), 1), (1, 0, 0, 1),
-              (random.random(), random.random(), random.random(), 1), (0, 1, 0, 1),
-              (random.random(), random.random(), random.random(), 1)]
-
-    main(number, cs)
